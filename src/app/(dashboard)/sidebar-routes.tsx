094305fe2052c734bef757a668440e3fb6195e5f
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-06-30 01:20:17 +0530

    added checks for pro user in sidebar and user button

--- src/app/(dashboard)/sidebar-routes.tsx
+++ src/app/(dashboard)/sidebar-routes.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -6,28 +6,38 @@ import { CreditCard, Crown, Home, MessageCircleQuestion } from "lucide-react";
 import React from "react";
 import SidebarItem from "./sidebar-item";
 import { usePathname } from "next/navigation";
+import { usePaywall } from "@/features/subscriptions/hooks/use-paywall";
+import { useCheckout } from "@/features/subscriptions/api/use-checkout";
 
 const SidebarRoutes = () => {
-  const pathname = usePathname()
+  const { shouldBlock, isLoading } = usePaywall();
+  const mutation = useCheckout();
+  const pathname = usePathname();
 
   return (
     <div className="flex flex-col gap-y-4 flex-1">
-      <div className="px-4 ">
-        <Button 
-          onClick={() => {}}
-          className="w-full rounded-xl border-none hover:bg-white hover:opacity-75 transition"
-          variant={"outline"}
-          size={"lg"}
-        >
-          <Crown className="mr-2 size-4 fill-yellow-500 text-yellow-500"/>
-          Upgrade to Pro
-        </Button>
-      </div>
-      <div className="px-3">
-        <Separator/>
-      </div>
-      <ul className="flex flex-col gap-y-1 px-3"> 
-        <SidebarItem 
+      {shouldBlock && !isLoading && (
+        <>
+          <div className="px-3">
+            <Button
+              onClick={() => mutation.mutate()}
+              disabled={mutation.isPending}
+              className="w-full rounded-xl border-none hover:bg-white hover:opacity-75 transition"
+              variant={"outline"}
+              size={"lg"}
+            >
+              <Crown className="mr-2 size-4 fill-yellow-500 text-yellow-500" />
+              Upgrade to Pro
+            </Button>
+          </div>
+          <div className="px-3">
+            <Separator />
+          </div>
+        </>
+      )}
+
+      <ul className="flex flex-col gap-y-1 px-3">
+        <SidebarItem
           href="/"
           icon={Home}
           label="Home"
@@ -35,16 +45,16 @@ const SidebarRoutes = () => {
         />
       </ul>
       <div className="px-3">
-        <Separator/>
+        <Separator />
       </div>
-      <ul className="flex flex-col gap-y-1 px-3"> 
-        <SidebarItem 
+      <ul className="flex flex-col gap-y-1 px-3">
+        <SidebarItem
           href={pathname}
           icon={CreditCard}
           label="Billing"
           onClick={() => {}}
         />
-        <SidebarItem 
+        <SidebarItem
           href="mailto:support@nyvo.com"
           icon={MessageCircleQuestion}
           label="Get Help"
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -8,10 +8,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { CreditCard, Loader, LogOut } from "lucide-react";
+import { usePaywall } from "@/features/subscriptions/hooks/use-paywall";
+import { CreditCard, Crown, Loader, LogOut } from "lucide-react";
 import { useSession, signOut } from "next-auth/react";
 
 const UserButton = () => {
+  const { shouldBlock, isLoading } = usePaywall();
+
   const session = useSession();
 
   if (session.status === "loading") {
@@ -27,8 +30,14 @@ const UserButton = () => {
 
   return (
     <DropdownMenu modal={false}>
-      <DropdownMenuTrigger>
-        {/* TODO: Add crown if user is premium */}
+      <DropdownMenuTrigger className="outline-none relative">
+        {!shouldBlock && !isLoading && (
+          <div className="absolute -top-1 -right-1 z-10 flex items-center justify-center">
+            <div className="rounded-full bg-white flex items-center justify-center p-1 drop-shadow-sm">
+              <Crown className="size-3 text-yellow-500 fill-yellow-500" />
+            </div>
+          </div>
+        )}
         <Avatar className="size-10 hover:opacity-75 transition cursor-pointer">
           <AvatarImage alt={name} src={imageUrl || ""} />
           <AvatarFallback className="bg-blue-500 font-medium text-white">
