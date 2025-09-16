ab656bd863fa3426dd617952810f40f34544242c
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-09-16 17:29:57 +0530

    Update banner.tsx

--- src/app/(dashboard)/banner.tsx
+++ src/app/(dashboard)/banner.tsx
@@ -27,7 +27,7 @@ const Banner = () => {
   };
 
   return (
-    <div className="text-white aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
+    <div className="text-white w-full max-w-full overflow-hidden aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
       <div className="rounded-full size-28 items-center justify-center bg-white/50 hidden md:flex">
         <div className="rounded-full size-20 flex items-center justify-center bg-white">
           <Sparkles className="h-20 text-[#0073ff] fill-[#0073ff]" />
